7d66419d82da108d36f2d882c05ff60681d13b3c
Keresztes-Nagy Bence <telrodan@gmail.com>
2023-04-10 11:30:50 +0200

    stripe payment added

--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -11,7 +11,7 @@ export class OrderService {
 
   constructor(private apiService: ApiService) {}
 
-  getCheckoutSession(shippingMethod) {
+  getCheckoutSession(shippingMethod: any) {
     return this.apiService.post('order/addOrder', shippingMethod);
   }
 }
