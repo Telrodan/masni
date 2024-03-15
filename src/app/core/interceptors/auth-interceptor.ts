8db6ea9a0f915f26d493c27f71b27ccfd49235b5
Telrodan <telrodan@gmail.com>
2024-03-15 20:29:06 +0100

    rework interceptors and remove spinner serivce

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,9 +1,9 @@
 import {
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-  HTTP_INTERCEPTORS
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest,
+    HTTP_INTERCEPTORS
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -13,19 +13,20 @@ import { CookieService } from '@core/services/cookie.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private cookieService: CookieService) {}
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    const authToken = this.cookieService.getCookie('token');
-    const authRequest = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + authToken)
-    });
-    return next.handle(authRequest);
-  }
+    constructor(private cookieService: CookieService) {}
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        const authToken = this.cookieService.getCookie('token');
+        const authRequest = req.clone({
+            headers: req.headers.set('Authorization', 'Bearer ' + authToken)
+        });
+
+        return next.handle(authRequest);
+    }
 }
 
 export const AuthInterceptorProvider = [
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
 ];
--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -1,49 +1,43 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpErrorResponse,
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-  HTTP_INTERCEPTORS
+    HttpErrorResponse,
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest,
+    HTTP_INTERCEPTORS
 } from '@angular/common/http';
 
-import { Observable, throwError, catchError, tap, finalize } from 'rxjs';
+import { Observable, throwError, catchError } from 'rxjs';
 
-import { SpinnerService } from '@core/services/spinner.service';
 import { ToastrService } from '@core/services/toastr.service';
 
 @Injectable()
 export class HTTPInterceptor implements HttpInterceptor {
-  constructor(
-    private spinnerService: SpinnerService,
-    private toastr: ToastrService
-  ) {}
+    constructor(private toastr: ToastrService) {}
 
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      tap(() => {
-        // this.spinnerService.startSpinner();
-      }),
-      catchError((error: HttpErrorResponse) => {
-        let errorMsg = '';
-        errorMsg = error.error.message;
-        if (errorMsg) {
-          this.toastr.error(errorMsg);
-        }
+    intercept(
+        request: HttpRequest<unknown>,
+        next: HttpHandler
+    ): Observable<HttpEvent<unknown>> {
+        return next.handle(request).pipe(
+            catchError((error: HttpErrorResponse) => {
+                let errorMsg = '';
+                errorMsg = error.error.message;
+                if (errorMsg) {
+                    this.toastr.error(errorMsg);
+                } else {
+                    this.toastr.error(
+                        'Hiba történt! Kérjük próbálja újra később!'
+                    );
+                }
 
-        return throwError(() => new Error(errorMsg));
-      }),
-      finalize(() => {
-        // this.spinnerService.stopSpinner();
-      })
-    );
-  }
+                return throwError(() => new Error(errorMsg));
+            })
+        );
+    }
 }
 
 export const HttpInterceptorProvider = [
-  { provide: HTTP_INTERCEPTORS, useClass: HTTPInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: HTTPInterceptor, multi: true }
 ];
