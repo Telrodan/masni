ade653ae16864ab279fbca68656f3929404bcaf5
Telrodan <telrodan@gmail.com>
2024-03-23 22:00:37 +0100

    rework doll dress dialog

--- src/app/features/shop/doll-dress-dialog/doll-dress-dialog.component.ts
+++ src/app/features/shop/doll-dress-dialog/doll-dress-dialog.component.ts
@@ -1,18 +1,22 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Product } from '@core/models/product.model';
-import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    HostBinding,
+    ViewEncapsulation
+} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
-    selector: 'mhd-doll-dress-dialog',
+    selector: 'nyk-doll-dress-dialog',
     standalone: true,
+    imports: [CommonModule, MatDialogModule, RouterModule],
     templateUrl: './doll-dress-dialog.component.html',
     styleUrls: ['./doll-dress-dialog.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None
 })
-export class DollDressDialogComponent implements OnInit {
-    products$: Observable<Product[]>;
-
-    ngOnInit(): void {
-        // this.products$ = this.store$.select(selectDollDresses);
-    }
+export class DollDressDialogComponent {
+    @HostBinding('class') hostClass = 'nyk-doll-dress-dialog';
 }
